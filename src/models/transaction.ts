eca9a959cb23e8ee8668df0100fbd05092751675
Alison Doyle <s00233102@atu.ie>
2024-10-16 17:29:27 +0100

    :construction: Adding validation to transaction model

--- src/models/transaction.ts
+++ src/models/transaction.ts
@@ -1,10 +1,23 @@
+import Joi from "joi";
+
 export interface Transaction {
     id:string;
     user:string;
     name:string;
     description:string;
-    category:string;
+    category?:string;
     date:Date;
     price:number;
     type:string;
+}
+
+export const ValidateTransaction = (transaction:Transaction) => {
+    const transactionSchema = Joi.object<Transaction>({
+        user: Joi.string().hex().length(24).required,
+        name: Joi.string().min(3).required(),
+        category: Joi.string().hex().length(24),
+        price: Joi.number().min(0).required(),
+    });
+
+    return transactionSchema.validate(transaction);
 }
