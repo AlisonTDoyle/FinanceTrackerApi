10bb8eb67bece3e8f8698cac50841c33a895a3ec
AlisonTDoyle <s00233102@atu.ie>
2024-12-22 21:01:25 +0000

    Change default server port to 10001 and add a simple Hello World route

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import cors from "cors";
 dotenv.config();
 
 // Express server setup
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10001;
 const app: Application = express();
 
 // Adding functionality
@@ -27,7 +27,11 @@ app.get("/test", async (_req: Request, res: Response) => {
     });
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 // Start server
 app.listen(PORT, () => {
-    console.log("Server is running on port  --", PORT);
-});
+    console.log(`Example app listening on port ${PORT}!`);
+})
